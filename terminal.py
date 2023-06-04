296d2ada8f5cbec567e5bec89c8730e10baf99fa
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-06-04 14:43:50 +0800

    still have problems

--- terminal.py
+++ terminal.py
@@ -1,23 +1,36 @@
 """"A Terminal Window"""
-from CustomTkinterTitlebar import CTT
+from CustomTkinterTitlebar import CTT, Image, ImageTk
 from sv_ttk import set_theme
 from tktermwidget import *
 from darkdetect import isDark
+from tkinter.ttk import Notebook, Button, Frame
 
 class TerminalWindow(CTT):
 	"""A Terminal Window"""
 	def __init__(self, ):
 		super().__init__()
-		
+
 		self.usetitle(False)
 		self.useicon(False)
-		self.useblur(True)
+		#self.useblur(True)
 		self.titlebarconfig(color={"color": "#2B2B2B", "color_nf": "#2f2f2f"}, height=40)
 		self.geometry("1000x515")
 		
+		self.notebook = Notebook(self.titlebar)
+		newtab_load = Image.open("newtab.png")
+		newtab_png = ImageTk.PhotoImage(newtab_load)
+		self.newtab = Button(self.titlebar, image=newtab_png, command=self.newtab)
 		self.term = Terminal(self)
-		self.term.pack(expand = True, fill = "both")
 		
+		self.term.pack(expand=True, fill="both")
+		self.notebook.pack(side="left", fill="x")
+		self.newtab.pack(side="left", fill="y", padx=3, pady=3)
+
+	def newtab(self):
+		newframe = Frame(self.notebook)
+		newframe.pack(fill="x", side="top")
+		self.notebook.add(newframe, text="New Tab")
+	
 if __name__ == "__main__":
 	example = TerminalWindow()
 	if isDark():
