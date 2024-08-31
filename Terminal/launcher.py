466faa203c880abf91e80168172118d69cdfe22f
小白云 <71159641+littlewhitecloud@users.noreply.github.com>
2024-08-31 21:48:19 +0800

    improve

--- Terminal/launcher.py
+++ Terminal/launcher.py
@@ -1,37 +1,42 @@
 from tkinter.ttk import Button
+from typing import Literal
 from customtitlebar import CTT
-from sv_ttk import set_theme
+from asset.sv_ttk import set_theme
 from tktermwidget import Terminal
 
 from widget import CTTNotebook, CTTNotebookFrame
 
 
-value = 0
+class TerminalWindow(CTT):
+    def __init__(self, theme: Literal["light", "dark", "auto"] = "auto") -> None:
+        super().__init__(theme)
 
-def newtab():
-    global value
+        self.value = 0
 
-    if value == 11:
-        return
-    else:
-        test = CTTNotebookFrame(ex)
-        terminal = Terminal(test.show)
-        terminal.pack(fill="both", expand=True)
-        test.show.pack(fill="both", expand=True)
-        nb.add_tab(test.dummy, test.show, text="term %d" % value)
-        value += 1
+        self.geometry("975x525")
+        self.titlebarconfig(height=40)
 
+        set_theme(self.theme)
 
-ex = CTT()
-ex.geometry("975x525")
-ex.titlebarconfig(height=40)
-ex.bg = "#2f2f2f"
-ex.setcolor()
+        self.nb = CTTNotebook(self.titlebar)
+        self.addtab = Button(self.titlebar, text=u'\uE109', command=self.newtab)
+        self.moreop = Button(self.titlebar, text=u'\uE015')
+        self.nb.pack(side="left", fill="x", padx=7)
+        self.addtab.pack(side="left", fill="y", padx=3, pady=5)
+        self.moreop.pack(side="left", fill="y", pady=5)
 
-set_theme(ex.theme)
+        self.mainloop()
 
-newtab = Button(ex.titlebar, text=''+u'\uE109', command=newtab)
-nb = CTTNotebook(ex.titlebar)
-nb.pack(side="left", fill="x", padx = 7)
-newtab.pack(side="left", fill="y", padx=3, pady=3)
-ex.mainloop()
+    def newtab(self):
+        if self.value == 11:
+            return
+        else:
+            test = CTTNotebookFrame(self)
+            terminal = Terminal(test.show)
+            terminal.pack(fill="both", expand=True)
+            test.show.pack(fill="both", expand=True)
+            self.nb.add_tab(test.dummy, test.show, text="term %d" % self.value)
+            self.value += 1
+
+test = TerminalWindow()
+test.mainloop()
